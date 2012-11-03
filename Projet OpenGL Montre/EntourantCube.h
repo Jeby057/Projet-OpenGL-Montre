e796bb1bf0cb050ebe04a1ea1e7611664870cdda
CyrPiers <cyril.piers@gmail.com>
2012-11-03 14:39:15 -0700

    Ajout des commentaires dans EntourantCube.h

--- Projet OpenGL Montre/EntourantCube.h
+++ Projet OpenGL Montre/EntourantCube.h
@@ -6,24 +6,29 @@
 class EntourantCube : public Piece
 {
 private:
-	double _largeurG;
-	double _longCoinG;
-	double _longHaut;
-	double _epaissG;
-	double _epaissD;
-	double _hauteur;
+	//////////// paramètres entrants
 
-	double _longNiveau;
-	double _longBas;
+	double _largeurG; // largeur de la pièce à gauche
+	double _longCoinG; // longueur des coins à gauche
+	double _longHaut; // longueur de la partie haute à gauche
+	double _epaissG; // épaisseur de la pièce à gauche
+	double _epaissD; // épaisseur de la pièce à droite
+	double _hauteur; // hauteur de la pièce (en profondeur)
+
+	double _longNiveau; // longueur du changement de niveau (milieu)
+	double _longBas; // longueur de la partie basse (milieu)
 	
-	double _prcLargPoseTri;
-	double _largeurD;
+	double _prcLargPoseTri; // pourcentage pris par la largeur de l'emplacement où est posé l'indicateur des minutes
 
-	double _angleCoinG;
-	double _angleNiveau;
-	double _angleInvNiveau;
+	//////////// autres variables locales
 
-	double _prctLargD;
+	double _largeurD; // largeur des faces du niveau bas à droite
+
+	double _angleCoinG; // angle du coin de gauche
+	double _angleNiveau; // angle du changement de niveau au milieu
+	double _angleInvNiveau; // angle du changement de niveau à droite
+
+	double _prctLargD; // pourcentage de la largeur des faces du niveau bas à droite (sur le reste de la largeur après soustraction de la largeur de l'emplacement où est posé l'indicateur des minutes
 
 public:
 	EntourantCube(double largeurG, double longCoinG, double longHaut, double epaissG, double epaissD, double longNiveau,
